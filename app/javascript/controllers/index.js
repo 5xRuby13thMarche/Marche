b5277427880a927586a3f660dc5a8385a111fdc8
Hsuan19971005 <g19971005@gmail.com>
2023-04-28 11:57:06 +0800

    Fix stimulus import error.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,14 +1,9 @@
-// Load all the controllers within this directory and all subdirectories. 
-// Controller files must be named *_controller.js.
+// // Load all the controllers within this directory and all subdirectories. 
+// // Controller files must be named *_controller.js.
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
-import Product__LikeController from "./product/like_controller"
-application.register("product--like", Product__LikeController)
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
