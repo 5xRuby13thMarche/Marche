bb1bc88cb0b1334d8cffe2c49960d447d6b1badc
Emily Ko <emily20000222@keio.jp>
2023-05-01 17:46:50 +0800

    refactor avatar helper to display resized SVG icon when user has no avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
     if user && user.avatar.present?
       image_tag user.avatar.variant(resize_to_fill: size).processed
     else
-      svg_tag = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-gray-200 " id="person"><g data-name="Layer 2"><path d="M12 11a4 4 0 1 0-4-4 4 4 0 0 0 4 4zm6 10a1 1 0 0 0 1-1 7 7 0 0 0-14 0 1 1 0 0 0 1 1z" data-name="person"></path></g></svg>'
+      svg_tag = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-gray-200 " id="person" width="' + size[0].to_s + '" height="' + size[1].to_s + '"><g data-name="Layer 2"><path d="M12 11a4 4 0 1 0-4-4 4 4 0 0 0 4 4zm6 10a1 1 0 0 0 1-1 7 7 0 0 0-14 0 1 1 0 0 0 1 1z" data-name="person"></path></g></svg>'
       # display the SVG icon as intended, rather than rendering it as plain text
       svg_tag.html_safe
     end
