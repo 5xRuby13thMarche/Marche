528b4c3894c0e9ab799429ae34cf5598550e2267
Emily Ko <emily20000222@keio.jp>
2023-05-01 17:39:21 +0800

    fix nil error in users_helper#avatar method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,11 @@
 module UsersHelper
   def avatar(user, size: [250, 250])
-    image_tag user.avatar.variant(resize_to_fill: size).processed if user.avatar.present?
+    if user && user.avatar.present?
+      image_tag user.avatar.variant(resize_to_fill: size).processed
+    else
+      svg_tag = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-gray-200 " id="person"><g data-name="Layer 2"><path d="M12 11a4 4 0 1 0-4-4 4 4 0 0 0 4 4zm6 10a1 1 0 0 0 1-1 7 7 0 0 0-14 0 1 1 0 0 0 1 1z" data-name="person"></path></g></svg>'
+      # display the SVG icon as intended, rather than rendering it as plain text
+      svg_tag.html_safe
+    end
   end
 end
