ffea02b9aef9a6581f161ff46c14cb378e53d760
Emily Ko <emily20000222@keio.jp>
2023-05-01 16:51:35 +0800

    add users reg controller. update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  
+  devise_for :users, controllers: { 
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
+ 
   # products
   root 'products#index'
   resources :products, except: [:index] do
