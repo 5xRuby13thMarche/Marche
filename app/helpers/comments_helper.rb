935dcc06e0c3fea53e955f00b538c8609b8b7f53
Hsuan19971005 <g19971005@gmail.com>
2023-05-04 12:00:34 +0800

    feat: Add average rating feature

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,5 @@ module CommentsHelper
   def set_comment_style(is_user_comment)
     is_user_comment ? "relative p-2 m-1 mb-2 bg-orange-100 border rounded border-b-slate-500" : "p-2 m-1 mb-2 border rounded bg-neutral-100 border-b-slate-500"
   end
+
 end
