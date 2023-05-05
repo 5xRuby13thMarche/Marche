1ae3c8d192aa79142548fc9870b2f5c6852429d7
Hsuan19971005 <g19971005@gmail.com>
2023-05-05 13:30:48 +0800

    perf: Update deleting cart-item feature by hotwire

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -25,7 +25,6 @@ class CartsController < ApplicationController
   def destroy
     @cart_product = CartProduct.find(params[:id])
     @cart_product.destroy()
-    redirect_to cart_path, notice: "已刪除商品"
   end
 
   private
