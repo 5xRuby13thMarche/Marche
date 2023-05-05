0c4a129da8403ae25b75e2156e20a64e5bcd8184
charlie51417 <charlie51417@gmail.com>
2023-05-05 13:37:04 +0800

    fix: Fix stimulus index file

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,11 +10,17 @@ application.register("cart--form", Cart__FormController)
 import Cart__ItemController from "./cart/item_controller"
 application.register("cart--item", Cart__ItemController)
 
+import Category__AssignController from "./category/assign_controller"
+application.register("category--assign", Category__AssignController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import Product__CartProductController from "./product/cart_product_controller"
+application.register("product--cart-product", Product__CartProductController)
 
 import Product__LikeController from "./product/like_controller"
 application.register("product--like", Product__LikeController)
 
-import QuantityController from "./quantity_controller"
-application.register("quantity", QuantityController)
+import Product__SaleInfoController from "./product/sale_info_controller"
+application.register("product--sale-info", Product__SaleInfoController)
