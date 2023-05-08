bf8acc65f49817d365b9ade04506da579d93eabe
Frank <t0970612806>
2023-05-08 12:27:07 +0800

    refactor: Done with Newebpay user_ email setting

--- app/servise/newebpay/mpg.rb
+++ app/servise/newebpay/mpg.rb
@@ -30,15 +30,15 @@ module Newebpay
     end
 
     def set_info(params)
-      info[:MerchantID] = @merchant_id   #勿動
-      info[:MerchantOrderNo] = params[:MerchantOrderNo]   #訂單編號，可更改
-      info[:Amt] = params[:Amt]               #金額 需更改  order.amount之類的
-      info[:ItemDesc] = params[:ItemDesc]        #商品名稱 需更改   order.name之類的
-      # info[:Email] = "kg936512@yahoo.com.tw"    #客戶的信箱，非常的莫名其妙，就算關閉此欄位，藍新結帳頁面依然會出現，須填完才可送出完成交易，所以需更改“
+      info[:MerchantID] = @merchant_id
+      info[:MerchantOrderNo] = params[:MerchantOrderNo]
+      info[:Amt] = params[:Amt]
+      info[:ItemDesc] = params[:ItemDesc]
+      info[:Email] = params[:Email]
       info[:TimeStamp] = Time.now.to_i   
-      info[:RespondType] = "JSON"        #勿動
-      info[:Version] = "2.0"             #勿動
-      info[:LoginType] = 0               #勿動
+      info[:RespondType] = "JSON"     
+      info[:Version] = "2.0"          
+      info[:LoginType] = 0            
       info[:CREDIT] =  1
       info[:ReturnURL] = "https://#{ENV['domain']}/orders/notify"
     end
