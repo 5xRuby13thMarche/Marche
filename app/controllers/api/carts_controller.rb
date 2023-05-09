56cbc203782d7ca57f05c8360e520bcf3ee015a9
Hsuan19971005 <g19971005@gmail.com>
2023-05-09 20:38:15 +0800

    feat: Add deleting all cart items feature

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,5 +1,5 @@
 class Api::CartsController < ApplicationController
-
+  # 購物車內商品數量的增減
   def edit
     @cart_product = CartProduct.find(params[:id])
     storage = @cart_product.sale_info.storage
@@ -11,6 +11,13 @@ class Api::CartsController < ApplicationController
       @cart_product.save
       render json: {state: "success"}
     end
+  end
+
+  # 清空購物車所屬的所有products
+  def destroy_items
+    cart = Cart.find(params[:cart_id])
+    cart.cart_products.destroy_all
+    render json: {state: "success"}
   end
   
 end
--- app/javascript/controllers/cart/form_controller.js
+++ app/javascript/controllers/cart/form_controller.js
@@ -1,9 +1,17 @@
 import {Controller} from "@hotwired/stimulus";
 import {formatMoney, convertMoneyToNumber} from "./application";
+import Swal from "sweetalert2";
+import {post, destroy} from "@rails/request.js";
 
 // Connects to data-controller="cart--form"
 export default class extends Controller {
-  static targets = ["checkbox", "itemTotalPrice", "totalPrice", "productNum"];
+  static targets = [
+    "checkbox",
+    "itemTotalPrice",
+    "totalPrice",
+    "productNum",
+    "cartItem",
+  ];
   connect() {
     // form表單預設會找尋第一個submit or button進行click事件，因此要preventDefault
     this.element.setAttribute(
@@ -44,5 +52,49 @@ export default class extends Controller {
       }
     }
     this.update();
+  }
+  clearAllCartItems(e) {
+    e.preventDefault();
+    // sweet alert
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton:
+          "border py-1 px-2 mx-2 rounded bg-lime-600 text-white hover:bg-lime-700",
+        cancelButton:
+          "border py-1 px-2 mx-2 rounded bg-red-400 text-white hover:bg-red-500",
+      },
+      buttonsStyling: false,
+    });
+    swalWithBootstrapButtons
+      .fire({
+        title: "清空購物車？",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes!",
+        cancelButtonText: "No!",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire("成功刪除");
+          // 前後端刪除購物車內商品
+          this.deleteAllCartItems();
+        }
+      });
+  }
+  // 刪除所有購物車項目
+  async deleteAllCartItems() {
+    // 前端畫面購物車內商品刪除
+    const cartLength = this.cartItemTargets.length;
+    for (let i = cartLength - 1; i >= 0; i--) this.cartItemTargets[i].remove();
+    // 打API刪除後端購物車內的所有商品
+    let url = `/api/carts/delete_all_items`;
+    const response = await destroy(url, {
+      body: JSON.stringify({cart_id: this.element.dataset.cartId}),
+    });
+    if (response.ok) {
+      const data = await response.json;
+      // console.log(data);
+    }
   }
 }
