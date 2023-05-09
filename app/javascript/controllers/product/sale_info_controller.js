a1ca8e224a441d2dd03036ec873f265a57db6997
charlie51417 <charlie51417@gmail.com>
2023-05-10 03:30:57 +0800

    fix: Add saleInfo form can't create

--- app/javascript/controllers/product/sale_info_controller.js
+++ app/javascript/controllers/product/sale_info_controller.js
@@ -4,20 +4,37 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["saleInfo"];
   connect(){
-    // this.copyStr = this.element.innerHTML.replace('data-product--sale-info-target="saleInfo"', 'data-product--sale-info-target="template"')
-    // console.log(this.copyStr)
+    this.formAmount = 1
+    this.formIndex = 1
   }
+  // 新增規格欄位最多三個
   increase(e) {
     e.preventDefault();
-    const content = this.saleInfoTarget.cloneNode(true);
-    console.log(content);
-    this.saleInfoTarget.insertAdjacentElement("afterend", content);
+    if (this.formAmount < 3){
+      const cloneContent = this.saleInfoTarget.cloneNode(true);
+      // 將節點轉為HTML字串
+      let content = cloneContent.outerHTML;
+      // console.log(content)
+      // 找到字串中[0]並替換為[1]
+      content = content.replace(/\[(\d+)\]/g, (match, p1) => `[${parseInt(p1)+this.formIndex}]`)
+      // 找到字串中_0_並替換為_1_
+      content = content.replace(/_(\d+)_/g, (match, p1) => `_${parseInt(p1)+this.formIndex}_`);
+      // 將字串轉為HTML節點
+      const container = document.createElement('div')
+      container.innerHTML = content;
+      const newContent = container.firstChild
+      this.saleInfoTarget.insertAdjacentElement("afterend", newContent);
+      this.formIndex += 1
+      this.formAmount += 1
+    }
   }
   
   decrease(e) {
     e.preventDefault();
-    if (this.element.children.length > 1){
+    console.log(this.saleInfoTarget)
+    if (this.element.children.length > 2){
       e.target.closest("div.deleteFields").remove();
+      this.formAmount = this.formAmount - 1
     }
   }
 }
