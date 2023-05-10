5acf52b91ab33e4766c82f3830d750887e57ee48
Hsuan19971005 <g19971005@gmail.com>
2023-05-10 22:07:27 +0800

    build: Deploy web by fly.io

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Marche
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.assets.css_compressor = nil
   end
 end
+
