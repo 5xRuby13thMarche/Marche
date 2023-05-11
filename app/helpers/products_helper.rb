f12a2c8ee6c3d5178c24ef4306f545e31cd6e495
kevinlee <caminolee@gmail.com>
2023-05-11 17:18:14 +0800

    style: Update product card layout,fix photo aspect ratio

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -16,7 +16,14 @@ module ProductsHelper
   end
   def image(product, size: [250, 250])
     if product.images.present?
-        image_tag product.images[0].representation(resize_to_limit: [400, 400]), class:"w-full rounded-t-sm aspect-1"
+        image_tag product.images[0].representation(resize_to_limit: [400, 400]), class:"mx-auto w-full rounded-t-sm aspect-1 object-cover"
+    else
+      image_tag 'https://cdn.iconscout.com/icon/free/png-512/free-user-1851010-1568997.png?f=avif&w=256', size: "#{size[0]}x#{size[1]}", class:"mx-auto w-full rounded-t-sm aspect-1 object-cover"
+    end
+  end
+  def show_image(product, size: [250, 250])
+    if product.images.present?
+        image_tag product.images[0].representation(resize_to_limit: [800, 800]), class:"mx-auto w-full rounded-t-sm aspect-1 object-cover"
     else
       image_tag 'https://cdn.iconscout.com/icon/free/png-512/free-user-1851010-1568997.png?f=avif&w=256', size: "#{size[0]}x#{size[1]}"
     end
