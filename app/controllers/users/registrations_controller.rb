d594e04afb0d89de68eb8767e2f53cbc2e5eb3d1
Emily Ko <emily20000222@keio.jp>
2023-05-14 14:36:15 +0800

    feat:Add name field for user editing and auto-generate unique username on sign up using Faker gem

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -10,9 +10,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super do |resource|
+      resource.name = "@#{Faker::Internet.password(min_length: 8)}"
+      resource.save
+    end
+  end
 
   # GET /resource/edit
   # def edit
@@ -41,13 +44,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :name])
   end
 
   # The path used after sign up.
