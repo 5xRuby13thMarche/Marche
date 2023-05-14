82c342bd8c375ea91c9b4cbcec605ada0c373fa5
Frank <t0970612806>
2023-05-14 15:20:49 +0800

    refactor: pull branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
+<<<<<<< HEAD
   before_action :authenticate_user!
 
   def show_like
     @products = current_user.liked_products
   end
+=======
+  # before_action :authenticate_user!
+
+  # def show_orders
+  #   @orders = current_user.orders
+  # end
+>>>>>>> 2ffd8f4 (refactor: pull branch)
 
   
 end
