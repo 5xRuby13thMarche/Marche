76c9eeedf54a1e9a495a3cf78fd9b7785358eaf4
Frank <t0970612806>
2023-05-15 22:02:44 +0800

    refactor: Fix navbar&footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,9 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
   before_action :authenticate_user!
 
   def show_like
     @products = current_user.liked_products
   end
-=======
-  # before_action :authenticate_user!
-
-  # def show_orders
-  #   @orders = current_user.orders
-  # end
->>>>>>> 2ffd8f4 (refactor: pull branch)
 
   
 end
