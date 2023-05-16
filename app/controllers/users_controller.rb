8f64e780ab3f5a84bd054ee75f2bf7b8342ec799
Hsuan19971005 <g19971005@gmail.com>
2023-05-16 12:09:27 +0800

    feat: Add pagination to user orders and liked_products

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show_like
-    @products = current_user.liked_products
+    @products = current_user.liked_products.includes(images_attachments: :blob).includes(:sale_infos)
+    @pagy, @products = pagy(@products, items: 18)
   end
 end
