4eddeeddad18ce6bdeecb699a461fca42abc3fd9
Hsuan19971005 <g19971005@gmail.com>
2023-05-17 16:52:44 +0800

    chore: Add back link to product show page

--- app/javascript/scripts/font-awesome.js
+++ app/javascript/scripts/font-awesome.js
@@ -7,11 +7,12 @@ import {faHeart as solidHeart} from "@fortawesome/free-solid-svg-icons";
 import {faTrashCan} from "@fortawesome/free-regular-svg-icons";
 import {faX} from "@fortawesome/free-solid-svg-icons";
 import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import {faBackward} from "@fortawesome/free-solid-svg-icons";
 
 library.add(regularHeart);
 library.add(solidHeart);
 library.add(faTrashCan);
 library.add(faX);
 library.add(faMagnifyingGlass);
-
+library.add(faBackward);
 dom.watch();
