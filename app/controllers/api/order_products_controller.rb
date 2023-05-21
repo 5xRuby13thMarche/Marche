55d6c06ad3ae209e570cd853b70607c877a94abd
Emily Ko <emily20000222@keio.jp>
2023-05-21 16:06:02 +0800

    feat: Implement email notification for 'Deliver Product' button (sandbox domain)

--- app/controllers/api/order_products_controller.rb
+++ app/controllers/api/order_products_controller.rb
@@ -5,6 +5,8 @@ class Api::OrderProductsController < ApplicationController
     order_product = OrderProduct.find(params[:id])
     if order_product.shipping_status == "not_shipped"
       order_product.update(shipping_status: params[:shipping])
+      OrderMailer.notify_delivery(order_product).deliver_now
+
       render json: {ok: 'shipped！', signInState: 'true'}
     else
       render json: {ok: 'product is already shipped！', signInState: 'true'}
