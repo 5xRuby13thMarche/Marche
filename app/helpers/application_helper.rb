4e4d40774ad584a6a9da3e226b783b5e5c9902af
Emily Ko <emily20000222@keio.jp>
2023-05-22 18:55:12 +0800

    feat: Add status translation methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,20 @@
 module ApplicationHelper
   include Pagy::Frontend
 
-  def shipped(shipping_status)
-    if shipping_status == "not_shipped"
-      return "未出貨"
-    else
-      return "已出貨"
-    end
+  def translate_payment_status(payment_status)
+    translations = {
+    "pending" => "未付款",
+    "paid" => "已付款"
+    }
+    translations[payment_status]
+  end
+
+  def translate_shipping_status(shipping_status)
+    translations = {
+    "pending" => "未出貨",
+    "shipped" => "已出貨",
+    "not_shipped" => "未出貨"
+    }
+    translations[shipping_status]
   end
 end
