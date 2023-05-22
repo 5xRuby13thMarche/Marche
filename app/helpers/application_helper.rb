75abb0e83dcda3d2d0e5a50f6115b40d8cfc8227
charlie51417 <charlie51417@gmail.com>
2023-05-22 17:27:08 +0800

    fix: shipping_status text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def shipped(shipping_status)
+    if shipping_status == "not_shipped"
+      return "未出貨"
+    else
+      return "已出貨"
+    end
+  end
 end
