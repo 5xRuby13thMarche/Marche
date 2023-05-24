f25d4677923a1316acdda9b1f2ef3c1289ec982b
Emily Ko <emily20000222@keio.jp>
2023-05-24 19:00:50 +0800

    fix: Update email content in notify_delivery.html.erb

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,7 @@
 class OrderMailer < ApplicationMailer
   def notify_delivery(order_product)
     @user = order_product.order.user
+    @order_product = order_product
     mail to:@user.email, subject:"Marche 出貨通知"
   end
 end
